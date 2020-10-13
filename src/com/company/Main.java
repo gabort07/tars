95257dd6d7ea9cf89695bd50a8cd81ca16083cd5
gabort07 <gabort07@hotmail.com>
2020-10-13 15:16:01 +0200

    ;

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,7 +25,7 @@ public class Main {
         int counter = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] % 2 == 1) {
-                counter++
+                counter++;
             }
         }
         return counter;
