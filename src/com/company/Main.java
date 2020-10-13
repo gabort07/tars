a19068b54a4857394abd145b13c108b266b8be23
gabort07 <gabort07@hotmail.com>
2020-10-13 14:30:51 +0200

    Szia Norbi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int[] randomArray =
+	int[] randomArray = new int[20];
+
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = (int) (Math.random() *100 + 1);
+        }
     }
 }
