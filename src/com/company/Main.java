52843469cb5b07aa70d7bc9889bdc61b854ddafb
NOBIPROG <ispannorbert@gmail.com>
2020-10-13 14:57:55 +0200

    oddNUms

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int[] randomArray = new int[20];
+        int[] randomArray = new int[20];
 
         for (int i = 0; i < randomArray.length; i++) {
-            randomArray[i] = (int) (Math.random() *100 + 1);
+            randomArray[i] = (int) (Math.random() * 100 + 1);
         }
         System.out.println(maxOfArrey(randomArray));
     }
+
     public static int maxOfArrey(int[] nums) {
         int max = nums[nums.length - 1];
         for (int i = 0; i < nums.length; i++) {
@@ -20,5 +21,15 @@ public class Main {
             }
         }
         return max;
+    }
+
+    public static int oddNums(int[] nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                counter++
+            }
+        }
+        return counter;
     }
 }
